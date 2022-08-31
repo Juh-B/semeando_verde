744567f8716aeedd2a20d8a88bd846b11ed3ffcb
Mauricio Pocas <mbpocas@hotmail.com>
2022-08-31 16:52:46 -0300

    Controller Gardenplant ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :gardens, only: %I[show edit update] do
-    resources :garden_plants, only: %I[create edit update]
+  resources :gardens, only: %I[show edit update]
+
+  resources :plants, only: %I[index show] do
+    resources :garden_plants, only: :create
   end
 
-  resources :plants, only: %I[index show]
-
-  resources :garden_plants, only: :destroy
+  resources :garden_plants, only: %i[destroy edit update]
 end
