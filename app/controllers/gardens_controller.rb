2524ef8158800155a6546a022610fe399f0fa033
Juliana Bonardi <julicbonardi@hotmail.com>
2022-09-01 18:10:47 -0300

    edit e update do nome do jardim

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -5,4 +5,26 @@ class GardensController < ApplicationController
     @garden = current_user.garden
     @garden_plants = @garden.garden_plants
   end
+
+  def edit
+    @garden = Garden.find(params[:id])
+  end
+
+  def update
+    @garden = Garden.find(params[:id])
+    respond_to do |format|
+      if @garden.update(garden_params)
+        format.html { redirect_to garden_url(@garden), notice: "Your garden was successfully updated." }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@garden)}_form", partial: "form", locals: { garden: @garden }) }
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def garden_params
+    params.require(:garden).permit(:name, :url)
+  end
 end
--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,4 +2,6 @@ class Garden < ApplicationRecord
   belongs_to :user
   has_many :garden_plants
   has_many :plants, through: :garden_plants
+
+  has_one_attached :photo
 end
