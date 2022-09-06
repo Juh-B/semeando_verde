0d739837233652db658f394b850dfae52423f2d8
Mauricio Pocas <mbpocas@hotmail.com>
2022-09-06 10:20:48 -0300

    Email notificação ok

--- app/models/garden_plant.rb
+++ app/models/garden_plant.rb
@@ -1,9 +1,11 @@
 class GardenPlant < ApplicationRecord
   belongs_to :garden
   belongs_to :plant
-
   # has_one_attached :photo
 
+  after_update :send_mail
+
   def send_mail
+    NotificationMailer.with(garden_plant: self).notification.deliver_now if notification
   end
 end
