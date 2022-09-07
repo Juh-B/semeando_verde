7d48094651b1bbedb0d4624842056d83c0c748d4
Aurelio Melo <aureliomelo003@gmail.com>
2022-09-07 13:15:19 -0300

    graba and drop added

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,6 +3,5 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application"
-
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import SortableController from "./sortable_controller"
+application.register("sortable", SortableController)
---
+++ db/migrate/20220907143649_add_order_to_garden_plants.rb
@@ -0,0 +1,5 @@
+class AddOrderToGardenPlants < ActiveRecord::Migration[7.0]
+  def change
+    add_column :garden_plants, :order, :integer
+  end
+end
