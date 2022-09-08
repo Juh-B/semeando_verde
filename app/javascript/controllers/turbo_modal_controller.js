bef47d0762cab14d4a113a8844c20ad984076fbc
Juliana Bonardi <julicbonardi@hotmail.com>
2022-09-08 17:11:17 -0300

    modal fix no garden_plant

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -1,8 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
-    if (this.element.classList.contains('modal-close')) { this.element.classList.remove('modal-close') }
-    this.element.classList.add('modal-open')
+    // if (this.element.classList.contains('modal-close')) { this.element.classList.remove('modal-close') }
+    // this.element.classList.add('modal-open')
     document.addEventListener('turbo:submit-end', this.handleSubmit);
   }
   disconnect() {
@@ -10,12 +10,12 @@ export default class extends Controller {
   }
   closeModal() {
 
-    if (this.element.classList.contains('modal-open'))
-     { this.element.classList.remove('modal-open') }
-    else
-      {return }
+    // if (this.element.classList.contains('modal-open'))
+    //  { this.element.classList.remove('modal-open') }
+    // else
+    //   {return }
 
-    this.element.classList.add('modal-close')
+    // this.element.classList.add('modal-close')
     setTimeout(() => {
       // Removes the src reference from turbo-frame
       // If this doesn't get removed, the modal won't open again
@@ -23,11 +23,11 @@ export default class extends Controller {
         this.element.closest('turbo-frame').src = undefined;
       }
       // Removes the modal
-      document.querySelector('form').submit();
       this.element.remove();
     }, 300);
   }
   handleSubmit = (event) => {
+    document.querySelector('form').submit();
     if (event.detail.success) {
       this.closeModal();
     }
